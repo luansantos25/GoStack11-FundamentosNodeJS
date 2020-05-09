2d507d6b3b1124a1081d9b383cf850f8c38363b5
Luan <luan.dev.santos@gmail.com>
2020-05-09 19:09:27 -0300

    Final challenge

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,12 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +20,26 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const balance = this.transactions.reduce<Balance>(
+      (acc: Balance, item: Transaction) => {
+        return { ...acc, [item.type]: acc[item.type] + item.value };
+      },
+      { income: 0, outcome: 0, total: 0 } as Balance,
+    );
+
+    return { ...balance, total: balance.income - balance.outcome };
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, type, value }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,15 +1,45 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
+interface Balance {
+  income: number;
+  outcome: number;
+  total: number;
+}
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
+  private balance: Balance;
+
   constructor(transactionsRepository: TransactionsRepository) {
     this.transactionsRepository = transactionsRepository;
+    this.balance = this.transactionsRepository.getBalance();
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ title, type, value }: Request): Transaction {
+    if (!['income', 'outcome'].includes(type))
+      throw Error('Invalid Transaction');
+
+    const isValidTransaction = this.balance.total >= value || type === 'income';
+
+    if (!isValidTransaction) {
+      throw Error('Yout do not have enough balance');
+    }
+
+    const transaction = this.transactionsRepository.create({
+      title,
+      type,
+      value,
+    });
+
+    return transaction;
   }
 }
 
